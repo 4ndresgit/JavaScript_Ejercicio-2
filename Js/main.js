beb5a71589309884d51dbbfaa2c093f4f6c4ead6
Andres Rossi <andresrossi2022@gmail.com>
2022-09-05 16:59:19 -0300

    Tarea JvaScript Ejercicio 2

---
+++ Js/main.js
@@ -0,0 +1,11 @@
+const listaNumeros = [,25,39,324,1021,52,14,1,12];
+
+let numeroMayor = 0;
+
+for (let i = 0; i < listaNumeros.length; i++) {
+    if (listaNumeros [i] > numeroMayor) {
+        numeroMayor = listaNumeros [i];
+    }
+    }
+    console.log(" el numero mayor es " + numeroMayor)
+
